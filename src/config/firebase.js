afe4b5f61d95c451adfbfe0f17bffb83496eef41
Samrat <77772897+samrat2k03@users.noreply.github.com>
2023-04-09 10:09:27 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,9 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7M-aoiG78NIkrrr6v7Oy1gqEWRjcBo2Y",
-  authDomain: "student-hub-1.firebaseapp.com",
-  projectId: "student-hub-1",
-  storageBucket: "student-hub-1.appspot.com",
-  messagingSenderId: "345674055053",
-  appId: "1:345674055053:web:47720aa1b956b16ac091c4",
-  measurementId: "G-VTJLWQ9B98"
+
+  // put your firebase config from your firebase console
+  
 };
 
 export const app = initializeApp(firebaseConfig);
